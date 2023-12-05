21e72b404ae230d7364a1bf4ab290a7a6d51e703
Malek Kazal <ghazal.malek@gmail.com>
2023-12-05 18:44:35 -0500

    toggle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleToggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <>
       <div className='h-[5vh]'>
-        <div className='text-xl text-center text-gray-100 font-medium pt-4'>
+        <div className='text-xl text-center text-gray-100 font-medium pt-4 invisible md:visible'>
           <ul className='flex items-center justify-center gap-8 sm:gap-14 md:gap-16 lg:gap-20'>
             <li>
               <a href='/'>Home</a>
@@ -18,6 +26,34 @@ const Header = () => {
             </li>
           </ul>
         </div>
+
+        <input type='checkbox' id='checkbox' onClick={handleToggleMenu} />
+        <label htmlFor='checkbox' className='toggle md:hidden'>
+          <div className='bars' id='bar1'></div>
+          <div className='bars' id='bar2'></div>
+          <div className='bars' id='bar3'></div>
+        </label>
+
+        {showMenu === true ? (
+          <div className='text-xl text-center text-gray-100 font-medium pt-4 shadow-sm md:hidden'>
+            <ul className='flex flex-col items-center justify-center gap-4'>
+              <li>
+                <a href='/'>Home</a>
+              </li>
+              <li>
+                <a href='/about'>About</a>
+              </li>
+              <li>
+                <a href='/portfolio'>Portfolio</a>
+              </li>
+              <li>
+                <a href='/contact'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
