bf228fd2bd3104f8c0e18587973c4f163b9d14db
Malek Kazal <ghazal.malek@gmail.com>
2023-12-24 14:51:03 -0500

    more portfolio images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,26 +1,41 @@
-import image2 from "../assets/images/portfolio/2.png";
-import image3 from "../assets/images/portfolio/3.png";
+import image2 from "../assets/images/portfolio/2.jpg";
+import image3 from "../assets/images/portfolio/22.jpg";
 import image4 from "../assets/images/portfolio/4.jpg";
-import image5 from "../assets/images/portfolio/5.png";
-import image6 from "../assets/images/portfolio/6.png";
-import image7 from "../assets/images/portfolio/7.png";
-import image8 from "../assets/images/portfolio/8.png";
+import image5 from "../assets/images/portfolio/21.jpg";
+import image6 from "../assets/images/portfolio/23.jpg";
+import image7 from "../assets/images/portfolio/24.jpg";
+import image8 from "../assets/images/portfolio/30.jpg";
 import image9 from "../assets/images/portfolio/9.png";
-import image10 from "../assets/images/portfolio/10.png";
-import image11 from "../assets/images/portfolio/8.jpg";
+import image10 from "../assets/images/portfolio/31.jpeg";
+import image11 from "../assets/images/portfolio/28.jpg";
 import image12 from "../assets/images/portfolio/12.png";
-import image13 from "../assets/images/portfolio/13.png";
-import image14 from "../assets/images/portfolio/9.jpg";
-import image15 from "../assets/images/portfolio/10.jpg";
-import image16 from "../assets/images/portfolio/11.jpg";
+import image13 from "../assets/images/portfolio/32.jpg";
+import image14 from "../assets/images/portfolio/29.jpg";
+import image15 from "../assets/images/portfolio/26.jpg";
+import image16 from "../assets/images/portfolio/27.jpg";
 import image17 from "../assets/images/portfolio/17.png";
 import image18 from "../assets/images/portfolio/18.png";
 import image19 from "../assets/images/portfolio/19.png";
-import image20 from "../assets/images/portfolio/12.jpg";
+import image20 from "../assets/images/portfolio/25.jpg";
 import image21 from "../assets/images/portfolio/13.jpg";
 import image22 from "../assets/images/portfolio/14.png";
 import image23 from "../assets/images/portfolio/15.jpg";
 import image24 from "../assets/images/portfolio/16.png";
+import image25 from "../assets/images/portfolio/33.jpg";
+import image26 from "../assets/images/portfolio/34.jpg";
+import image27 from "../assets/images/portfolio/35.jpg";
+import image28 from "../assets/images/portfolio/36.jpg";
+import image29 from "../assets/images/portfolio/37.jpg";
+import image30 from "../assets/images/portfolio/38.jpg";
+import image31 from "../assets/images/portfolio/39.jpg";
+import image32 from "../assets/images/portfolio/40.jpg";
+import image33 from "../assets/images/portfolio/41.jpg";
+import image34 from "../assets/images/portfolio/42.jpg";
+import image35 from "../assets/images/portfolio/43.jpg";
+import image36 from "../assets/images/portfolio/44.jpg";
+import image37 from "../assets/images/portfolio/45.jpg";
+import image38 from "../assets/images/portfolio/46.jpg";
+import image39 from "../assets/images/portfolio/47.jpg";
 
 const Portfolio = () => {
   return (
@@ -68,6 +83,21 @@ const Portfolio = () => {
           image22,
           image23,
           image24,
+          image25,
+          image26,
+          image27,
+          image28,
+          image29,
+          image30,
+          image31,
+          image32,
+          image33,
+          image34,
+          image35,
+          image36,
+          image37,
+          image38,
+          image39,
         ].map((image, index) => (
           <div key={index} className='w-full md:w-1/2 lg:w-1/4 xl:w-1/4 mb-8'>
             <img
